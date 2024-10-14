3b62854c6114ba4e25551c7f50d98d45749d873b
maryamsiddique <maryamsiddique860@gmail.com>
2024-10-15 01:24:50 +0500

    update

---
+++ src/app/page.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="Parentdiv">
+
+
+      <div className="leftpane">
+
+        <h1 className="text">hey!</h1>
+        <h1 className="text1"> My name is Maryam Siddique</h1>
+        <h1 className="text2">I am a nextjs developer
+        </h1>
+
+    </div>
+
+    <div className="rightpane">
+    <img
+          src="/images/maryam.jpg" 
+          alt="My image" 
+          width={170} 
+          height={100} 
+        />
+
+
+      </div>
+
+
+    </div>
+
+    
+    
+  )
+}
